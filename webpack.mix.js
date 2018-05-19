9ed4631aec4649e160d24c70d557f513c2d49686
Mederic Roy Beldia <twocngdagz@yahoo.com>
2018-05-19 11:37:31 +0800

    update laravel mix for assets handling

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,25 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'resources/assets/css');
+mix.copy('node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css', 'resources/assets/css');
+
+
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js');
+mix.copy('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'resources/assets/js');
+mix.copy('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js', 'resources/assets/js');
+
+
+mix.styles([
+        'resources/assets/css/bootstrap.min.css',
+        'resources/assets/css/dataTables.bootstrap4.css'
+    ], 'public/css/app.css');
+
+mix.scripts([
+        'resources/assets/js/app.js',
+        'resources/assets/js/jquery.min.js',
+        'resources/assets/js/bootstrap.bootstrap.bundle.min.js',
+        'resources/assets/js/dataTables.bootstrap4.js'
+    ], 'public/js/app.js');
+
+
